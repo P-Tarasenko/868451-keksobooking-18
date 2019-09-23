f1420fcc1f6ba95dd580910066a2a7fdff27ea02
P-Tarasenko <46486793+P-Tarasenko@users.noreply.github.com>
2019-09-23 22:38:44 +1000

    Промежуточный этап готовности ДЗ, созданы все функции

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
+'use strict';
+
 var COUNT = 8;
+var HALF_WIDTH_PIN = 25;
+var HALF_HEIGHT_PIN = 35;
 var map = document.querySelector('.map');
+var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+var listOfPins = document.querySelector('.map__pins');
 var featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photosArr = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var types = ['palace', 'flat', 'house', 'bungalo'];
 var time = ['12:00', '13:00', '14:00'];
 
 var getRandomNumber = function (max) {
- return Math.round(Math.random() * max);
+  return Math.round(Math.random() * max);
 };
 var getRandomInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
+};
 var createRandomArr = function (arr) {
   var list = [];
   for (var e = 0; e < getRandomNumber(arr.length - 1); e++) {
@@ -40,12 +45,27 @@ var createData = function () {
         'photos': createRandomArr(photosArr)
       },
       'location': {
-        'x': getRandomNumber(document.documentElement.clientWidth),
+        'x': (getRandomNumber(document.documentElement.clientWidth) - 25),
         'y': getRandomInRange(130, 630)
       }
-    }
+    };
   }
   return data;
 };
-
+var data = createData();
+var createPins = function (arr) {
+  var similarPin = pin.cloneNode(true);
+  similarPin.setAttribute('style', 'left: ' + (arr.location.x - HALF_WIDTH_PIN) + 'px; ' + 'top: ' + (arr.location.y - HALF_HEIGHT_PIN) + 'px;');
+  similarPin.setAttribute('src', arr.author.avatar);
+  similarPin.setAttribute('alt', arr.offer.title);
+  return similarPin;
+};
+var addPins = function (arr) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < arr.length; i++) {
+    fragment.appendChild(createPins(arr[i]));
+  }
+  listOfPins.appendChild(fragment);
+};
+addPins(data);
 map.classList.remove('map--faded');
