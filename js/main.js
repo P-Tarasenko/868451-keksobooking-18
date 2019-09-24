e4d554c56f4f4b3dd12776e8e44c54320b2615a1
P-Tarasenko <46486793+P-Tarasenko@users.noreply.github.com>
2019-09-24 15:08:57 +1000

    Исправил код по замечаниям наставника

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var HALF_WIDTH_PIN = 25;
 var HALF_HEIGHT_PIN = 35;
 var map = document.querySelector('.map');
 var pin = document.querySelector('#pin').content.querySelector('.map__pin');
-var listOfPins = document.querySelector('.map__pins');
+var listOfPins = map.querySelector('.map__pins');
 var featuresArr = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photosArr = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var types = ['palace', 'flat', 'house', 'bungalo'];
@@ -14,9 +14,11 @@ var time = ['12:00', '13:00', '14:00'];
 var getRandomNumber = function (max) {
   return Math.round(Math.random() * max);
 };
+
 var getRandomInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
 var createRandomArr = function (arr) {
   var list = [];
   for (var e = 0; e < getRandomNumber(arr.length - 1); e++) {
@@ -24,22 +26,27 @@ var createRandomArr = function (arr) {
   }
   return list;
 };
-var createData = function () {
+
+var getRandomItem = function (arr) {
+  return arr[getRandomNumber(arr.length - 1)];
+};
+
+var createData = function (count) {
   var data = [];
   for (var i = 0; i < COUNT; i++) {
     data[i] = {
       'author': {
-        'avatar': 'img/avatars/user0' + getRandomNumber(8) + '.png'
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
       },
       'offer': {
         'title': 'Заголовок',
         'address': '' + getRandomNumber(600) + ', ' + getRandomNumber(500),
         'price': getRandomInRange(10000, 100000),
-        'type': types[getRandomNumber(types.length - 1)],
+        'type': getRandomItem(types),
         'rooms': getRandomNumber(4),
         'guests': getRandomNumber(10),
-        'checkin': time[getRandomNumber(2)],
-        'checkout': time[getRandomNumber(2)],
+        'checkin': getRandomItem(time),
+        'checkout': getRandomItem(time),
         'features': createRandomArr(featuresArr),
         'description': 'Описание',
         'photos': createRandomArr(photosArr)
@@ -52,20 +59,26 @@ var createData = function () {
   }
   return data;
 };
-var data = createData();
-var createPins = function (arr) {
+
+var data = createData(COUNT);
+
+var createPin = function (obj) {
   var similarPin = pin.cloneNode(true);
-  similarPin.setAttribute('style', 'left: ' + (arr.location.x - HALF_WIDTH_PIN) + 'px; ' + 'top: ' + (arr.location.y - HALF_HEIGHT_PIN) + 'px;');
-  similarPin.setAttribute('src', arr.author.avatar);
-  similarPin.setAttribute('alt', arr.offer.title);
+  var img = similarPin.querySelector('img');
+  similarPin.style.left = (obj.location.x - HALF_WIDTH_PIN) + 'px';
+  similarPin.style.top = (obj.location.y - HALF_HEIGHT_PIN) + 'px';
+  img.setAttribute('src', obj.author.avatar);
+  img.setAttribute('alt', obj.offer.title);
   return similarPin;
 };
+
 var addPins = function (arr) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < arr.length; i++) {
-    fragment.appendChild(createPins(arr[i]));
+    fragment.appendChild(createPin(arr[i]));
   }
   listOfPins.appendChild(fragment);
 };
+
 addPins(data);
 map.classList.remove('map--faded');
